f98fabbd6318f21d8f2dcbb2c21656b929298231
SkyzJrz <janottamaap@gmail.com>
2025-12-02 15:25:42 +0700

    Ignore node_modules for Vercel build

--- dev-dist/sw.js
+++ dev-dist/sw.js
@@ -82,7 +82,7 @@ define(['./workbox-54d0af47'], (function (workbox) { 'use strict';
     "revision": "d41d8cd98f00b204e9800998ecf8427e"
   }, {
     "url": "index.html",
-    "revision": "0.5dnmhdd8qvo"
+    "revision": "0.43v53hons2o"
   }], {});
   workbox.cleanupOutdatedCaches();
   workbox.registerRoute(new workbox.NavigationRoute(workbox.createHandlerBoundToURL("index.html"), {
