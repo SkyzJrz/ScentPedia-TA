bbac540f7b133b8628deb26ea3a25b981932b70d
SkyzJrz <janottamaap@gmail.com>
2025-12-04 17:24:46 +0700

    update

--- dev-dist/sw.js
+++ dev-dist/sw.js
@@ -82,7 +82,7 @@ define(['./workbox-54d0af47'], (function (workbox) { 'use strict';
     "revision": "d41d8cd98f00b204e9800998ecf8427e"
   }, {
     "url": "index.html",
-    "revision": "0.43v53hons2o"
+    "revision": "0.vodgj475i7o"
   }], {});
   workbox.cleanupOutdatedCaches();
   workbox.registerRoute(new workbox.NavigationRoute(workbox.createHandlerBoundToURL("index.html"), {
